bc6c09e2d20bf78158d77d8831d8e85b2f81a954
Nitesh Kartha <karthanitesh@gmail.com>
2019-06-05 09:43:19 -0500

    Finished documentation for JMidiControl.

--- src/JMidiControl.java
+++ src/JMidiControl.java
@@ -84,47 +84,61 @@ public class JMidiControl {
     // will be changed to toDescriptiveString in version 0.2
     @Override
     public String toString() {
+        // Creates a StringBuilder and a MIDIControlMessage
         StringBuilder stringBuilder = new StringBuilder();
         MidiControlMessage message = null;
 
+        // End of Track has no extra information
         if(codeNumber == END_OF_TRACK){
             return "MIDI END OF TRACK";
         }
 
+        // Finds the resulting MIDIControlMessage for the code number
         for(MidiControlMessage mes: MESSAGE_SET){
             if(mes.codeNum == codeNumber){
                 message = mes;
             }
         }
 
+        // Just in case but this should never be run.
         if(message == null)
             return "MIDI Control Change NOT FOUND";
 
+        // Otherwise, we simply record what the MIDIControlMessage stores
         stringBuilder.append("MIDI Control Change: ");
         stringBuilder.append(message.codeName);
 
+        // It cannot be on/off
         if(message.offLimit == 0){
             // off limit doesn't really exist
             return stringBuilder.toString();
         }
 
+        // Otherwise, we see whether it is on or off
         if(currentStatus <= message.offLimit)
             stringBuilder.append(" OFF");
         else
             stringBuilder.append(" ON");
 
+        // Then we return the whole thing
         return stringBuilder.toString();
     }
 
-
-
+    // Inner class to represent the objects contained in MESSAGE_SET. These objects
+    // are generated using initMessageSet().
     private static class MidiControlMessage implements Comparable<MidiControlMessage> {
+        // Instance variables for the name of the MIDI Control Message, the number
+        // of the code, the on limit (which represents how high a number can be for
+        // it to be considered an "ON" message) and the off limit (how low a number
+        // can be for it to be considered an "OFF" message).
+        // If onLimit == offLimit == 0, then the MIDI Control Message does not have
+        // an on/off data bit.
         private String codeName;
         private int codeNum;
         private int offLimit;
         private int onLimit;
 
-
+        // Basically assigns everything to the object.
         public MidiControlMessage(int codeNumber, int offLim, int onLim, String codeName) {
             codeNum = codeNumber;
             offLimit = offLim;
@@ -132,6 +146,7 @@ public class JMidiControl {
             this.codeName = codeName;
         }
 
+        // compareTo is necessary in order to allow for a TreeSet.
         @Override
         public int compareTo(MidiControlMessage o) {
             return codeNum - o.codeNum;
