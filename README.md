56b71b14b537e1f92f10e4baee874b54100eb3de
Nitesh Kartha <karthanitesh@gmail.com>
2019-06-18 15:43:18 -0500

    Finished all documentation in code and updated README for public use.

--- README.md
+++ README.md
@@ -19,6 +19,14 @@ However, more importantly, MIDI *does not store actual audio of a song/music*. R
 
 Java has a very robust MIDI [library](https://docs.oracle.com/javase/tutorial/sound/overview-MIDI.html) but since its main purpose is to allow developers to create MIDI programs rather than analyze the MIDI files themselves, JMidi utilizes the Java's MIDI libararies and gears them towards data analysis.
 
+### Version 0.1 (*JMIDI Larghessimo*) ###
 
+This is the first release of JMidi that is currently released as a beta version. 
+Major changes such as adopting a more efficient format for output in order to allow
+for better data analysis as well as support for measuring counting and "declutter" mode
+(which aims to reduce the amount of MIDI messages recorded to those you wish to keep track of)
+is coming soon.
 
-### *** UNDER CONSTRUCTION *** 
+Similarly, a user guide that explains the library in more detail is in the works.
+
+### In-depth Documentation coming soon! ###
--- src/JMidiTimeSign.java
+++ src/JMidiTimeSign.java
@@ -1,18 +1,29 @@
+/* Class that represents a MIDI Time Signature message.
+   Allows client to no longer require original MIDI data to
+   determine what the time signature at a given tick/note is.
+ */
 public class JMidiTimeSign {
+    // Instance variables representing the numerator, denominator,
+    // ticks per Metronome Click, and 32nd notes per MIDI Quarter Note (should be 8).
     private int numerator;
     private int denominator;
     private int midiTicksPerMetronomeClick;
     private int _32ndNotesPerMidiQuarterNote;
 
+    // Constructor using MIDI data.
     public JMidiTimeSign(byte[] data){
-        assert data.length == 4; // to follow MIDI guidelines
+        // Must have all four values for it to be a valid Time Signature Message
+        if(data.length != 4)
+            throw new IllegalArgumentException("MIDI data for Time Signature must be 4 bytes long");// to follow MIDI guidelines
 
         numerator = data[0];
-        denominator = ((int) Math.pow(2, data[1]));
+        denominator = ((int) Math.pow(2, data[1])); // denom = 2^second data value
         midiTicksPerMetronomeClick = data[2];
         _32ndNotesPerMidiQuarterNote = data[3];
     }
 
+    // Prints it out in a human readable (though inefficient) manner,
+    // will be changed to toDescriptiveString in version 0.2
     @Override
     public String toString() {
         StringBuilder stringBuilder = new StringBuilder();
